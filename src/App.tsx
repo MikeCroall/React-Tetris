d5bf383d99a797f99c6904b16bd804e46828e2d3
georgeprice <georgepriiice@gmail.com>
2018-08-30 21:57:18 +0100

    Using the new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import Gameboard from './components/viewport/gameboard';
+import Scoreboard from './components/viewport/scores';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App"/>
+      <div className="App">
+        <Scoreboard />
+        <Gameboard />
+      </div>
     );
   }
 }
