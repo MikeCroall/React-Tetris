9cf9ed0d7a29585170c1610d61aab290e0c42de1
georgeprice <georgepriiice@gmail.com>
2018-08-30 21:57:04 +0100

    Added React components to display to the user

---
+++ src/components/viewport/gameboard/index.tsx
@@ -0,0 +1,43 @@
+import { css } from 'aphrodite';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { IStore } from '../../../reducers/rootReducer';
+import styles from './styles';
+
+/** 
+ * Gameboard needs an array of 2d grid layers, and WxH dimensions
+ */
+export interface IGameboardProps {
+    grid: boolean[][];
+}
+
+/** 
+ * Displays a set of grid information 
+ */
+export class Gameboard extends React.Component<IGameboardProps> {
+
+    constructor(props: IGameboardProps){
+        super(props);
+    }
+
+    public render() {
+        return (
+            <div className={css(styles.grid)}>
+                {
+                    this.props.grid.map((row, y) => (
+                        <div className={css(styles.row)} key={y} >
+                            {
+                                row.map((cell, x) => 
+                                <div className={css(styles.cell, cell ? styles.active : null)} key={x + '' + y} />)
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default connect( (state: IStore) => ({
+    grid: state.foreground.merge(state.background).getCells()
+}))(Gameboard);
---
+++ src/components/viewport/scores/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { IStore } from '../../../reducers/rootReducer';
+
+export interface IScoreboardProps {
+    score: number
+}
+
+export class Scoreboard extends React.Component<IScoreboardProps> {
+
+    constructor(props: IScoreboardProps){
+        super(props);
+    }
+
+    public render() {
+        return (
+            <p> {this.props.score} </p>
+        );
+    }
+}
+
+export default connect( (state: IStore) => (console.log(state), {
+    score: state.score
+}))(Scoreboard);
