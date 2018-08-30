afcba7f9cb62e4a46df5c7ab8abf54cdd41de47d
georgeprice <georgepriiice@gmail.com>
2018-08-30 16:34:50 +0100

    Redux store testing, tidying up code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
-import { Grid } from './components/grid/';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <Grid width={10} height={30}/>
-      </div>
+      <div className="App"/>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,23 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import { spawnTetromino } from './actions';
 import App from './App';
+import { Tetromino } from './components/tetromino';
 import rootReducer from './reducers/rootReducer';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+
+store.dispatch(spawnTetromino(Tetromino.BACKWARDS_Z));
+
+unsubscribe()
 
 ReactDOM.render(
   <Provider store={store}>
